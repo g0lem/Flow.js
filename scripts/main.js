40ff96fcba08f3aa7fcb717fdd0826e70f8141e7
g0lem <sasteve999@gmail.com>
2017-11-26 15:39:07 +0200

    TODO: Make directives work as it goes along, don't make them execute when you have all of them, it's stupid because you don't know when the developer has stopped pushing them from his app

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 /**
  *      Flow.js - a portable frontend Model View Controller made in JavaScript
  *
- *      made by: Serb Stefan & Gabriel Apostol
+ *      made by: Stefan Serb & Gabriel Apostol
  *      26/11/2017
  *
  */
@@ -11,19 +11,45 @@ var app = {
     directives: []
 };
 
+/**
+ *  The logic of the directives is to be loaded, and execute the functions based on the querySelector in pure JavaScript
+ *  the Flow app should be able to load modules as it goes, it will have a basic execute script at the beginning to
+ *  load all the basic functionality, but after that it should still be able to get directives and controllers from
+ *  the apps that use the framework
+ */
+
+//TODO: make this execute as it goes, not once everything is loaded
+
+//Push directives and execute the functions if needed
+app.directive = function(name, func){
+    app.directives.push({ name: name, func: func});
+}
+
 document.onreadystatechange = function () {
     if (document.readyState === 'complete') {
         executeFrameworkModules();
     }
 };
 
-
+//Execute everything, once the
 var executeFrameworkModules = function () {
+    getAllDirectives();
+    executeAllDirectives();
+}
+
+var appDirective = function(){
     var a = document.querySelector('[' + frameworkAppDirectiveName + ']');
     var appName = app.name = a.attributes.getNamedItem(frameworkAppDirectiveName).value;
     console.log(appName);
 }
 
-app.directive = function(name, func){
-    directives.push({ name: name, func: func});
+var executeAllDirectives = function(){
+    for(var i in app.directives){
+        app.directives[i].func();
+    }
+}
+
+
+var getAllDirectives = function(){
+    app.directive("flw-app", appDirective);
 }
